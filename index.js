63e843e2b96b252d711af399e3f21ace73ce871b
Markus Kannisto <m.kannisto01@gmail.com>
2025-08-17 20:36:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -67,13 +67,13 @@ app.get('/oauth/success', async (req, res) => {
 
         res.redirect(process.env.REDIRECT_URI);
     } catch (e) {
-        res.status(400).json({ success: false, error: e.message });
+        res.redirect(process.env.APPWRITE_REDIRECT_URI_ERROR + `error=${encodeURIComponent(e.message)}`);
     }
 });
 
 app.get('/oauth/failure', (req, res) => {
     const errorMessage = req.query.error || "OAuth failed";
-    res.status(400).json({ success: false, error: errorMessage });
+    res.redirect(process.env.APPWRITE_REDIRECT_URI_ERROR + `error=${encodeURIComponent(errorMessage)}`);
 });
 
 // Upload a file
