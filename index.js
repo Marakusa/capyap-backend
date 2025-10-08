a842af0a953b7930987f2e3650eee1e77bda3fa5
Markus Kannisto <m.kannisto01@gmail.com>
2025-10-08 14:15:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ const authorBufferSize = 128; // 128 bytes for settings
 // Middleware to limit repeated requests to public APIs and/or endpoints
 const limiter = rateLimit({
 	windowMs: 60 * 1000, // 1 minute
-	limit: 30, // Limit each IP to 100 requests per `window` (here, per a minute).
+	limit: 60, // Limit each IP to 100 requests per `window` (here, per a minute).
 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	ipv6Subnet: 58, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
